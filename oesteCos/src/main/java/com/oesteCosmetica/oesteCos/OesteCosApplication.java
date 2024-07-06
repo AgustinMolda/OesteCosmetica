f5ab7a29e8667e6e9821bba8feb96cbafadb4e2e
Agustin Moldavsky <agusting.moldavsky64gmail.com>
2024-07-05 23:45:55 -0300

    ApplicationStart

---
+++ oesteCos/src/main/java/com/oesteCosmetica/oesteCos/OesteCosApplication.java
@@ -0,0 +1,13 @@
+package com.oesteCosmetica.oesteCos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OesteCosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OesteCosApplication.class, args);
+	}
+
+}
---
+++ oesteCos/src/test/java/com/oesteCosmetica/oesteCos/OesteCosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oesteCosmetica.oesteCos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OesteCosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
