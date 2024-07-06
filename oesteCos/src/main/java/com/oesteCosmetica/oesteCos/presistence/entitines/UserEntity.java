9f8a30703996d5bb03f0aefc6d4952ca58f44950
Agustin Moldavsky <agusting.moldavsky64gmail.com>
2024-07-06 00:33:48 -0300

    feature/userEntity

---
+++ oesteCos/src/main/java/com/oesteCosmetica/oesteCos/presistence/entitines/UserEntity.java
@@ -0,0 +1,31 @@
+package com.oesteCosmetica.oesteCos.presistence.entitines;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String username;
+    private String password;
+
+
+}
