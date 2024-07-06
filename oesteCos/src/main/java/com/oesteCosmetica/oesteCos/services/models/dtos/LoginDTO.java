be79e4337af94465a14b58c4da187ee954a3cc2e
Agustin Moldavsky <agusting.moldavsky64gmail.com>
2024-07-06 00:41:42 -0300

    feature/DTOSCreated

---
+++ oesteCos/src/main/java/com/oesteCosmetica/oesteCos/services/models/dtos/LoginDTO.java
@@ -0,0 +1,12 @@
+package com.oesteCosmetica.oesteCos.services.models.dtos;
+
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+
+    private String userName;
+    private String password;
+
+
+}
---
+++ oesteCos/src/main/java/com/oesteCosmetica/oesteCos/services/models/dtos/ResponseDTO.java
@@ -0,0 +1,9 @@
+package com.oesteCosmetica.oesteCos.services.models.dtos;
+
+import lombok.Data;
+
+@Data
+public class ResponseDTO {
+    private int numOfErrors;
+    private String message;
+}
