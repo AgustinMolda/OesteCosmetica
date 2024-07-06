2f89b3ce0ab6b6f9575a3bc452a550489883dd2a
Agustin Moldavsky <agusting.moldavsky64gmail.com>
2024-07-06 00:38:47 -0300

    feature/userRepository

---
+++ oesteCos/src/main/java/com/oesteCosmetica/oesteCos/presistence/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.oesteCosmetica.oesteCos.presistence.repositories;
+
+import com.oesteCosmetica.oesteCos.presistence.entitines.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository  extends JpaRepository<UserEntity,Long> {
+
+    @Query(value = "SELECT * FROM user WHERE username = :username", nativeQuery = true)
+    Optional<UserEntity> findByUsername(String username);
+}
